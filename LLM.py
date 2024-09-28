8e2da8fbe22a885b4cc0e272573830524b78da0c
Avet <crykrypta@gmail.com>
2024-09-28 16:07:15 +0300

    Добавлен функционал для ДЗ PRO

--- LLM.py
+++ LLM.py
@@ -19,6 +19,7 @@ openai.api_key = config.openai_key
 class LLM:
     # Инициализация класса
     def __init__(self, path_to_base: str,
+                 system: str | None = None,
                  chunk_size: int = 1024,
                  model_name: str = 'gpt-3.5-turbo',
                  metadata_lenths=True):
@@ -26,13 +27,12 @@ class LLM:
 
         Args:
             path_to_base (str): _description_
+            system (str | None, optional): _description_. default: None.
             chunk_size (int, optional): _description_. default: 1024.
             model_name (str, optional): description_. def-lt: 'gpt-3.5-turbo'.
             metadata_lenths (bool, optional): _description_. default: True.
             _type_: _description_
         """
-        # Сохраняем параметры модели
-        self.model_name = model_name
 
         # Загружаем текст базы
         with open(path_to_base, 'r', encoding='utf-8') as file:
@@ -53,19 +53,48 @@ class LLM:
         embeddings = OpenAIEmbeddings()
         self.db = FAISS.from_documents(source_chunks,
                                        embeddings)  # type:ignore
+        # Сохраняем системный промпт
+        self.system = system
+        # Сохраняем параметры модели
+        self.model_name = model_name
 
-    # Базовый системный промпт
-    default_system = '''Ты - нейроконсультант, ответь на
-    вопрос пользователя на основе документа с информацией.\n
-    Не придумывай ничего от себя, отвечай максимально по документу.\n
-    Не упоминай Документ с информацией для ответа пользователю.\n
-    Пользователь ничего не должен знать про
-    Документ с информацией для ответа пользователю'''
+    # Внутренняя функция для более читабельного вывода модели
+    def _format_text(self, text: str, limit: int) -> str:
+        words = text.split()
+        formatted_text = []
+        current_line = ""
+
+        for word in words:
+            # Проверяем, поместится ли следующее слово в текущую строку
+            if len(current_line) + len(word) + 1 <= limit:
+                # Если текущая строка пустая, добавляем слово без пробела
+                if current_line:
+                    current_line += " " + word
+                else:
+                    current_line = word
+            else:
+                # Добавляем строку в список и начинаем новую
+                formatted_text.append(current_line)
+                current_line = word
+
+        # Добавляем последнюю строку, если она не пустая
+        if current_line:
+            formatted_text.append(current_line)
+
+        # Возвращаем результат в виде строк, разделённых символом новой строки
+        return "\n".join(formatted_text)
 
     # Функция получения ответа от ChatGPT
     def get_answer(self, query: str,
-                   system: str = default_system,
-                   temperature: float = 0.0) -> str | None:
+                   temperature: float = 0.0,
+                   outline_limit: int = 100) -> str | None:
+        # Базовый системный промпт
+        system = self.system or '''Ты - нейроконсультант, ответь на
+            вопрос пользователя на основе документа с информацией.\n
+            Не придумывай ничего от себя, отвечай максимально по документу.\n
+            Не упоминай Документ с информацией для ответа пользователю.\n
+            Пользователь ничего не должен знать про
+            Документ с информацией для ответа пользователю'''
 
         # Релевантные отрезки из базы
         docs = self.db.similarity_search(query, k=4)
@@ -87,4 +116,6 @@ class LLM:
             messages=messages,  # type:ignore
             temperature=0
         )
-        return completion.choices[0].message.content
+
+        return self._format_text(completion.choices[0].message.content,
+                                 outline_limit)
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from LLM import LLM
 # Инициализация класса LLM
 llm = LLM('Simble.txt')
 
+# Счетчик обращений к LLM
+counter = 0
+
 # Инициализация FastAPI
 app = FastAPI()
 
@@ -38,6 +41,8 @@ async def sum(
     a: float = Query(..., description='Первое число для сложения'),
     b: float = Query(..., description='Второе число для сложения')
 ):
+    global counter
+    counter += 1
     return {'answer': a + b}
 
 
@@ -47,6 +52,8 @@ async def subtract(
     a: float = Query(..., description='Первое число для вычитания'),
     b: float = Query(..., description='Второе число для вычитания')
 ):
+    global counter
+    counter += 1
     return {'answer': a - b}
 
 
@@ -56,6 +63,8 @@ async def multiply(
     a: float = Query(..., description='Первое число для умножения'),
     b: float = Query(..., description='Второе число для умножения')
 ):
+    global counter
+    counter += 1
     return {'answer': a * b}
 
 
@@ -67,4 +76,13 @@ async def divide(
 ):
     if b == 0:
         raise HTTPException(status_code=400, detail='Деление на ноль')
+    global counter
+    counter += 1
     return {'answer': a / b}
+
+
+# Счетчик обращений к LLM
+@app.get('/calc/counter')
+async def get_counter():
+    global counter
+    return {'counter': counter}
